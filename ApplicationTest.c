1cd296e116c59e19e2ff7eeb1b34220ee89cdd2a
JuanPabloLoCoco <jlococo@itba.edu.ar>
2018-03-30 16:48:17 -0300

    Update ApplicationTest.c

--- ApplicationTest.c
+++ ApplicationTest.c
@@ -1,86 +1,139 @@
 //ApplicationTest
+//ApplicationTest
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <dirent.h> 
 #include <stdio.h> 
 #include <string.h>
+#include <dirent.h> 
 
 #define NULL ((void *)0)
+#define MAX_NAMEPATH 255
 
-void testSearchFileAndList();
+void givenAPath();
+void whenPathIsGiven();
+void thenVerifyPathIsCorrect();
+void correctFileTest();
 
+void push(char* char);
+char* pop();
 
-void givenAFile();
-void whenFileFound();
-void thenAddToList();
-
-char * examplePath=NULL;
-DIR * file=NULL;
-
-struct 
+typedef struct
 {
-	/* data */
-};
+    char* path;
+    struct node *next;
+}stackNodeCDT;
 
+struct stackNodeCDT *top=NULL;
+
+
+char * examplePath =NULL;
+DIR * file=NULL;
+struct dirent* dir;
 
 int main(int args, char *argv[])
 {
-	printf("Test Application: ");
-	testTakeADirectory();
+	printf("Testeo de archivos dados: ");
+	correctFileTest();
 
+	printf("Testeo de toma de un archivo: ");
+	takeAFileTest();
 
-// ImpletarQueue
-	testAddAFileToFIFO();
-
-	testWriteOnFds();
-
-	testSaveOnDisk();
-
-	testManageState();
-
-	testKillChilds();
+	printf("Testeo de Envio:\n");
+	
 }
 
 
-void testTakeADirectory()
+void correctFileTest()
 {
 	givenAPath();
-
-	whenFileInPathFound();
-
-	thenVerifyIsADirectory();
+	whenPathIsGiven();
+	thenVerifyPathIsNotNUll();
 }
-
 
 void givenAPath()
 {
 	examplePath=".";
 }
 
-void whenFileInPathFound()
+void whenPathIsGiven()
 {
 	file=opendir(examplePath);
-
 }
 
-void thenVerifyIsADirectory (){
-	if (file==NULL){
-		perror("Zaraza");
+void thenVerifyPathIsNotNUll()
+{
+	checkIsNotNull((void*)file);
+}
+
+
+void checkIsNotNull(void * pointer)
+{
+	if (pointer == NULL)
+	{
+		fail("	Expected: a non null pointer, got: a null pointer");
 	}
-
-	struct dirent *dir;
-	dir=readdir(file);
-	
-
-	if (dir!=NULL){
-		if (isDirectory(dir)){
-			perror("ZARAZA");
-		}
+	else
+	{
+		ok();
 	}
-
 }
 
 
-int isDirectory(dirent *dir){
-	return (dir->d_type==DT_DIR)
+void ok()
+{
+	printf("	Ok!\n");
 }
+
+void fail(char * withError)
+{
+	putchar('\n');
+	fprintf(stderr, "%s", withError);
+}
+
+
+void makeStack(char* path)
+{
+	DIR* d=opendir(path);
+	struct dirent* dir;
+ 	while ((dir = readdir(d)) != NULL)
+    {
+    	if(dir-> d_type != DT_DIR) 
+        	char d_path[MAX_NAMEPATH]
+        	sprintf(d_path, "%s/%s", path, dir->d_name);
+        	push(d_path);
+      	else
+      	if(strcmp(dir->d_name,".")!=0 && strcmp(dir->d_name,"..")!=0 ) 
+      	{
+        	char d_path[MAX_NAMEPATH]; // here I am using sprintf which is safer than strcat
+        	sprintf(d_path, "%s/%s", path, dir->d_name);
+        	makeStack(d_path); // recall with the new path
+      	}
+    }
+}
+
+
+void push(char *item)
+{
+    struct node *nptr = malloc(sizeof(struct StackNodeCDT));
+    nptr->data;
+    nptr->next = top;
+    top = nptr;
+}
+
+int isEmpty()
+{
+	return top==NULL;
+}
+
+char * pop()
+{
+    
+    struct node *temp;
+    temp = top;
+    top = top->next;
+    char* resp=temp->path;
+    free(temp);
+    return resp;
+}
+
