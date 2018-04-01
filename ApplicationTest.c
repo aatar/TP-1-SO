ab3e63e08236dfa37e1f84abb3c48f2d5306ceac
gprinc <gprinc@itba.edu.ar>
2018-04-01 20:33:41 -0300

    added new tests

--- ApplicationTest.c
+++ ApplicationTest.c
@@ -9,16 +9,25 @@
 #define MAX_NAMEPATH 255
 
 void givenAPath();
+void givenTheFDS();
+
 void whenPathIsGiven();
+void whenPipesAreCreated();
+
 void thenVerifyPathIsCorrect();
-void correctFileTest();
 void thenVerifyPathIsNotNull();
 void thenCreateStack();
+void thenVerifyPipeIsCreated();
+void thenVerifyMultiplePipesAreCreated();
+
+void correctFileTest();
+void takeAFileTest();
+void pipeCreationTest();
+void creationSonTest();
 
 void checkIsNotNull(void * pointer);
 void ok();
 void fail(char * failError);
-void takeAFileTest();
 
 
 void push(char * path);
@@ -36,6 +45,13 @@ struct stackNodeCDT * top = NULL;
 char * examplePath = NULL;
 DIR * file = NULL;
 struct dirent * dir;
+int communicationPipes[4];
+int * communicationMultiplePipes;
+int pipeCreationFather;
+int pipeCreationSon;
+int multiplePipes[4];
+int sonsAmount = 2;
+
 
 int main()
 {
@@ -46,6 +62,11 @@ int main()
 	takeAFileTest();
 
 	// printf("Testeo de Envio:\n");
+  printf("Testeo de creacion de canales de comunicacion con Hijos:");
+  pipeCreationTest();
+
+  printf("Testeo de creacion de Hijos: ");
+  creationSonTest();
 
   return 0;
 }
@@ -110,7 +131,6 @@ void makeStack(char * path)
           char d_path[MAX_NAMEPATH];
         	sprintf(d_path, "%s/%s", path, dir->d_name);
         	push(d_path);
-          printf("%s\n",d_path);
       }
       else
       {
@@ -123,6 +143,7 @@ void makeStack(char * path)
       }
   }
 }
+
 
 
 void push(char * item)
@@ -159,5 +180,84 @@ void takeAFileTest()
 void thenCreateStack()
 {
   makeStack(examplePath);
-  ok();
+}
+
+void pipeCreationTest()
+{
+  givenTheFDS();
+  whenPipesAreCreated();
+  thenVerifyPipeIsCreated();
+  thenVerifyMultiplePipesAreCreated();
+}
+
+void givenTheFDS()
+{
+  /*
+  * communicationPipes 0,1 belongs Father to Son 1
+  * communicationPipes 2,3 belongs Son 1 to Father
+  */
+  communicationMultiplePipes = malloc(sizeof(int) * sonsAmount * 4);
+}
+
+void whenPipesAreCreated()
+{
+  // pipeCreationFather = pipe(communicationPipes);
+  // pipeCreationSon = pipe(communicationPipes + 2);
+  for(int i = 0; i < sonsAmount * 2; i++)
+  {
+    multiplePipes[i] = pipe(communicationMultiplePipes + (i * 2));
+  }
+}
+
+void thenVerifyPipeIsCreated()
+{
+  if(pipeCreationFather == -1)
+  {
+    fail("Father pipe not created");
+  }
+  else {
+    ok();
+  }
+  if (pipeCreationSon == -1)
+  {
+    fail("Son pipe not created");
+  }
+  else
+  {
+    ok();
+  }
+}
+
+void thenVerifyMultiplePipesAreCreated()
+{
+  // j is the number of pipes. To a communication between father and son belogns two pipes. To make full duplex connection.
+  for(int j = 0; j < sonsAmount * 2; j++)
+  {
+    if(multiplePipes[j] == -1)
+    {
+      fail("Father pipe not created");
+      putchar('\n');
+    }
+    else {
+      ok();
+    }
+    if (pipeCreationSon == -1)
+    {
+      fail("Son pipe not created");
+      putchar('\n');
+    }
+    else
+    {
+      ok();
+    }
+  }
+}
+
+
+void creationSonTest()
+{
+  givenAPipe();
+  int pid = whenSonIsCreated();
+  thenVerifySonCreation(pid);
+  thenCheckPipesChanges(pid);
 }
