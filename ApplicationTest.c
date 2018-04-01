d08fe1e1402390c7177f971bf23c58cdf8527040
gprinc <gprinc@itba.edu.ar>
2018-04-01 19:09:13 -0300

    ApplicationTest fixes

--- ApplicationTest.c
+++ ApplicationTest.c
@@ -1,37 +1,43 @@
 //ApplicationTest
-//ApplicationTest
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <dirent.h> 
-#include <stdio.h> 
+#include <dirent.h>
+#include <stdio.h>
 #include <string.h>
-#include <dirent.h> 
+#include <dirent.h>
 
-#define NULL ((void *)0)
 #define MAX_NAMEPATH 255
 
 void givenAPath();
 void whenPathIsGiven();
 void thenVerifyPathIsCorrect();
 void correctFileTest();
+void thenVerifyPathIsNotNull();
+void thenCreateStack();
 
-void push(char* char);
-char* pop();
+void checkIsNotNull(void * pointer);
+void ok();
+void fail(char * failError);
+void takeAFileTest();
 
-typedef struct
+
+void push(char * path);
+char * pop();
+
+struct stackNodeCDT
 {
-    char* path;
-    struct node *next;
-}stackNodeCDT;
+    char * path;
+    struct stackNodeCDT *next;
+};
 
-struct stackNodeCDT *top=NULL;
+struct stackNodeCDT * top = NULL;
 
 
-char * examplePath =NULL;
-DIR * file=NULL;
-struct dirent* dir;
+char * examplePath = NULL;
+DIR * file = NULL;
+struct dirent * dir;
 
-int main(int args, char *argv[])
+int main()
 {
 	printf("Testeo de archivos dados: ");
 	correctFileTest();
@@ -39,8 +45,9 @@ int main(int args, char *argv[])
 	printf("Testeo de toma de un archivo: ");
 	takeAFileTest();
 
-	printf("Testeo de Envio:\n");
-	
+	// printf("Testeo de Envio:\n");
+
+  return 0;
 }
 
 
@@ -48,22 +55,22 @@ void correctFileTest()
 {
 	givenAPath();
 	whenPathIsGiven();
-	thenVerifyPathIsNotNUll();
+	thenVerifyPathIsNotNull();
 }
 
 void givenAPath()
 {
-	examplePath=".";
+	examplePath = ".";
 }
 
 void whenPathIsGiven()
 {
-	file=opendir(examplePath);
+	file = opendir(examplePath);
 }
 
-void thenVerifyPathIsNotNUll()
+void thenVerifyPathIsNotNull()
 {
-	checkIsNotNull((void*)file);
+	checkIsNotNull((void *)file);
 }
 
 
@@ -92,48 +99,65 @@ void fail(char * withError)
 }
 
 
-void makeStack(char* path)
+void makeStack(char * path)
 {
-	DIR* d=opendir(path);
-	struct dirent* dir;
+	DIR * d = opendir(path);
+	struct dirent * dir;
  	while ((dir = readdir(d)) != NULL)
-    {
-    	if(dir-> d_type != DT_DIR) 
-        	char d_path[MAX_NAMEPATH]
+  {
+    	if(dir-> d_type != DT_DIR)
+      {
+          char d_path[MAX_NAMEPATH];
         	sprintf(d_path, "%s/%s", path, dir->d_name);
         	push(d_path);
-      	else
-      	if(strcmp(dir->d_name,".")!=0 && strcmp(dir->d_name,"..")!=0 ) 
+          printf("%s\n",d_path);
+      }
+      else
+      {
+      	if(strcmp(dir->d_name,".") != 0 && strcmp(dir->d_name,"..") != 0 )
       	{
         	char d_path[MAX_NAMEPATH]; // here I am using sprintf which is safer than strcat
         	sprintf(d_path, "%s/%s", path, dir->d_name);
         	makeStack(d_path); // recall with the new path
       	}
-    }
+      }
+  }
 }
 
 
-void push(char *item)
+void push(char * item)
 {
-    struct node *nptr = malloc(sizeof(struct StackNodeCDT));
-    nptr->data;
+    struct stackNodeCDT * nptr = malloc(sizeof(struct stackNodeCDT));
+    nptr->path = item;
     nptr->next = top;
     top = nptr;
 }
 
 int isEmpty()
 {
-	return top==NULL;
+	return top == NULL;
 }
 
 char * pop()
 {
-    
-    struct node *temp;
+    struct stackNodeCDT * temp;
     temp = top;
     top = top->next;
-    char* resp=temp->path;
+    char * resp = temp->path;
     free(temp);
     return resp;
 }
 
+
+void takeAFileTest()
+{
+  givenAPath();
+  whenPathIsGiven();
+  thenCreateStack();
+}
+
+void thenCreateStack()
+{
+  makeStack(examplePath);
+  ok();
+}
