6421f762e1b4b961b277e17636f0f7154db4cb9e
JuanPabloLoCoco <jlococo@itba.edu.ar>
2018-04-05 01:24:43 -0300

    Update viewProcess.c

--- viewProcess.c
+++ viewProcess.c
@@ -6,9 +6,27 @@
 #include <sys/stat.h>
 #include <stdlib.h>
 #include <signal.h>
-
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <semaphore.h>
 
 int main(int argc, char *argv[]) {
+  int shmsegid;
+  char *p, *s;
+  char c;
+
+  //Crear memoria
+  shmsegid = shmget(1237, 1024, IPC_CREAT | 0666);
+  if (shmsegid < 0)
+  {
+    perror("shmget failed: ");
+  }
+  //Attach memoria a nuestro address space
+  p = shmat(shmsegid, 0 , 0);
+  if (p == (char *) -1)
+  {
+    perror("shmat failed: ");
+  }
   int bucle = 1;
   sem_t *s = sem_open("/sem",O_RDWR);
 
